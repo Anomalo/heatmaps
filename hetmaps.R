245327c8bf5756ec1653881fceccc8a0c89dfd08
Albert <lahat.albert@gmail.com>
2015-03-10 12:02:19 +0000

    done

--- hetmaps.R
+++ hetmaps.R
@@ -1,11 +1,11 @@
 args<-commandArgs(TRUE)
+library(RColorBrewer)
 library(gplots)
 files = c('crossover.csv','fpkmnoduplicats.csv','postfed.csv')
 
 f = args
-
+print(f)
 pname = paste(f,'png',sep='.')
-print(pname)
 data <- read.table(f,check.names=FALSE,sep=",",header=TRUE,row.names=1)
 means = rowMeans(data)
 data = data[!means==0,]
@@ -17,18 +17,18 @@ data = data/means
 
 data[data==0] <- 0.00001
 data = log2(data)
-max = 3
+max = 5
 min = -max
 data[data<min] <- min
 data[data>max] <- max
 #memory.limit(30000)
 
-
+#data = head(data)
 data_matrix <- data.matrix(data)
-col = c("#B2182B", "#D6604D", "#F4A582", "#FDDBC7", "#F7F7F7", "#D1E5F0" ,"#92C5DE", "#4393C3" ,"#2166AC")
+col <- colorRampPalette(c("blue", "white", "red"))(n = 1000)
 png(pname,width=6,height=16,units="in",res=1000)
-par(mar=c(5,6,4,1)+.1)
-heatmap.2(data_matrix,key=NULL,keysize=2,trace = 'none',col=col,labRow='')
+heatmap.2(data_matrix,col=col,keysize=2,margins=c(7,1),trace = 'none',labRow='',Colv=FALSE)
 dev.off()
 
 
+print(pname)
