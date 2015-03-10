de65ede580e4188e25297ae4b8734fd373ed8320
Albert <lahat.albert@gmail.com>
2015-03-10 15:14:57 +0000

    s

--- hetmaps.R
+++ hetmaps.R
@@ -2,10 +2,11 @@ args<-commandArgs(TRUE)
 library(RColorBrewer)
 library(gplots)
 files = c('crossover.csv','fpkmnoduplicats.csv','postfed.csv')
+max = 10
 
 f = args
 print(f)
-pname = paste(f,'png',sep='.')
+pname = paste(f,max,'png',sep='.')
 data <- read.table(f,check.names=FALSE,sep=",",header=TRUE,row.names=1)
 means = rowMeans(data)
 data = data[!means==0,]
@@ -17,7 +18,6 @@ data = data/means
 
 data[data==0] <- 0.00001
 data = log2(data)
-max = 5
 min = -max
 data[data<min] <- min
 data[data>max] <- max
